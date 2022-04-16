65065e46b90b919f25746822e45f7fb389f9a1bf
hillarywolff <hwolff@uchicago.edu>
2022-04-16 15:26:14 -0500

    Update MP1_Wolff.py

--- MP1/MP1_Wolff.py
+++ MP1/MP1_Wolff.py
@@ -9,6 +9,9 @@ Created on Sat Apr 16 13:13:50 2022
 import pandas as pd
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+import seaborn as sns
 
 PATH = r"/Users/hillarywolff/Documents/GitHub/machine_learning/MP1/"
  
@@ -59,7 +62,8 @@ df['female']= np.where((df['SEX']==2), 1, 0)
 df['vet'] =np.where((df['VETSTAT']==2), 1, 0)
 
 df['EDUxHISPAN'] = df['EDUCDC']*df['hispanic']
-df['EDUxHSDIP'] = df['EDUCDC']*df['coldip']
+df['EDUxHSDIP'] = df['EDUCDC']*df['hsdip']
+df['EDUxCOLDIP'] = df['EDUCDC']*df['coldip']
 df['EDUxWHITE'] = df['EDUCDC']*df['White']
 df['EDUxBLACK'] = df['EDUCDC']*df['Black']
 df['EDUxMARRIED']= df['EDUCDC']*df['married']
@@ -68,6 +72,69 @@ df['EDUxVET'] = df['EDUCDC']*df['vet']
 
 df['age_sq'] = np.power(df['AGE'], 2)
 df['INCWAGE_log'] = np.log(df['INCWAGE'])
+
+describe_cols = ['EDUxHISPAN', 'EDUxHSDIP', 'EDUxCOLDIP', 'EDUxWHITE', 'EDUxBLACK',
+            'EDUxMARRIED', 'EDUxFEMALE', 'EDUxVET', 'vet', 'female', 'Black',
+            'White', 'hispanic', 'coldip', 'hsdip', 'EDUCDC', 'age_sq', 'AGE', 
+            'INCWAGE','INCWAGE_log', 'NCHILD', 'YEAR']
+
+reg_cols = ['EDUCDC', 'female', 'AGE', 'age_sq', 'White', 'Black', 
+            'hispanic', 'married', 'NCHILD', 'vet']
+reg_cols = ' + '.join(reg_cols)
+
+describe_df=pd.DataFrame()
+for col in describe_cols:
+    describe_df.append(df[col].describe())
+
+plt.scatter(df['EDUCDC'], df['INCWAGE_log'])
+plt.title('ln(wage) vs. Educational Attainment')
+plt.ylabel('ln(wage) ($10,000)')
+plt.xlabel('Education level')
+
+
+
+
+result = smf.ols('INCWAGE_log ~ {}'.format(reg_cols), data=df).fit().summary()
+results_as_html = result.tables[1].as_html()
+reg_df = (pd.read_html(results_as_html, header=0, index_col=0)[0]).reset_index()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
