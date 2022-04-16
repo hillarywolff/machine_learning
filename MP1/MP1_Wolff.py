6fafe5cebc4d3cda40b28c761b857d46b7e165ee
hillarywolff <hwolff@uchicago.edu>
2022-04-16 14:49:29 -0500

    Update MP1_Wolff.py

--- MP1/MP1_Wolff.py
+++ MP1/MP1_Wolff.py
@@ -6,3 +6,80 @@ Created on Sat Apr 16 13:13:50 2022
 @author: hillarywolff
 """
 
+import pandas as pd
+import os
+import numpy as np
+
+PATH = r"/Users/hillarywolff/Documents/GitHub/machine_learning/MP1/"
+ 
+
+
+def read_data(fname):
+    df = pd.read_csv(os.path.join(PATH, fname)) 
+    return df
+
+fname = 'usa_00001.csv'
+
+df = read_data(fname)
+
+crosswalk_fname = 'PPHA_30545_MP01-Crosswalk.csv'
+
+crosswalk = read_data(crosswalk_fname)
+crosswalk = crosswalk.set_index('educd').T
+crosswalk = crosswalk.to_dict('list')
+
+
+df['EDUCDC'] = df['EDUCD']
+df = df.replace({'EDUCDC':crosswalk})
+
+df['hsdip'] = np.where((df['EDUC']==62)| 
+                       (df['EDUCD']==63)|
+                       (df['EDUCD']==64), 1, 0)
+
+df['coldip'] = np.where((df['EDUCD']==101)|
+                        (df['EDUCD']==114)|
+                        (df['EDUCD']==115)|
+                        (df['EDUCD']==115)|
+                        (df['EDUCD']==116), 1, 0)
+
+df['White'] = np.where(df['RACE'] == 1, 1, 0)
+
+df['Black'] = np.where(df['RACE'] == 2, 1, 0)
+
+df['hispanic'] = np.where((df['HISPAN']==1)|
+                          (df['HISPAN']==2)|
+                          (df['HISPAN']==3)|
+                          (df['HISPAN']==4), 1, 0)
+
+df['married']= np.where((df['MARST']==1)|
+                        (df['MARST']==2), 1, 0)
+
+df['female']= np.where((df['SEX']==2), 1, 0)
+
+df['vet'] =np.where((df['VETSTAT']==2), 1, 0)
+
+df['EDUxHISPAN'] = df['EDUCDC']*df['hispanic']
+df['EDUxHSDIP'] = df['EDUCDC']*df['coldip']
+df['EDUxWHITE'] = df['EDUCDC']*df['White']
+df['EDUxBLACK'] = df['EDUCDC']*df['Black']
+df['EDUxMARRIED']= df['EDUCDC']*df['married']
+df['EDUxFEMALE'] = df['EDUCDC']*df['female']
+df['EDUxVET'] = df['EDUCDC']*df['vet']
+
+df['age_sq'] = np.power(df['AGE'], 2)
+df['INCWAGE_log'] = np.log(df['INCWAGE'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
